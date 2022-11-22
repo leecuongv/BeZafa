66ca3b43448bd747879a14c9411e708211cbe4f2
Lê Văn Cường <vancuongle65@gmail.com>
2022-11-22 03:10:10 +0000

    up

--- models/Chat.js
+++ models/Chat.js
@@ -2,14 +2,26 @@ const mongoose = require("mongoose");
 
 const chatModel = mongoose.Schema(
   {
-    chatName: { type: String, trim: true },
-    isGroupChat: { type: Boolean, default: false },
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    chatName: {
+      type: String,
+      trim: true
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false
+    },
+    users: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }],
     latestMessage: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message",
     },
-    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
   },
   { timestamps: true }
 );
