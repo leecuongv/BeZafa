9cad5defa833697be4bc158dad6d249b98b43d0e
ryuuwon <vancuongle65@gmail.com>
2022-11-23 10:30:56 +0700

    up

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -42,7 +42,7 @@ const AuthController = {
 
             temp = await User.findOne({ email: req.body.email,type:TYPE_ACCOUNT.NORMAL })
             if (temp) {
-                return res.status(400).json({username: "Email đã tồn tại" })
+                return res.status(400).json({message: "Email đã tồn tại" })
             }
             const activeCode = jwt.sign(
                 { email },
